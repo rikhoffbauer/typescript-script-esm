d2a1eb20e80802de78492a6a3bd7ab7bd5444b7b
Rik Hoffbauer <github.com@riks.email>
2024-08-04 09:24:43 +0200

    Create typescript.element.js

---
+++ typescript.element.js
@@ -0,0 +1,65 @@
+  class TypeScriptElement extends HTMLElement {
+            static get observedAttributes() {
+                return ['compilerOptions'];
+            }
+
+            constructor() {
+                super();
+                this.shadow = this.attachShadow({ mode: 'open' });
+            }
+
+            async connectedCallback() {
+                await this.loadTypeScript();
+                this.transpile();
+            }
+
+            attributeChangedCallback(name, oldValue, newValue) {
+                if (name === 'compilerOptions' && oldValue !== newValue) {
+                    this.transpile();
+                }
+            }
+
+            async loadTypeScript() {
+                if (window.ts) return;
+
+                const script = document.createElement('script');
+                script.src = 'https://unpkg.com/typescript@latest/lib/typescript.js';
+                script.onload = () => {
+                    this.transpile();
+                };
+                document.head.appendChild(script);
+            }
+
+            transpile() {
+                if (!window.ts) return;
+
+                const code = this.innerHTML.trim();
+              
+                try {
+                  const compilerOptionsStr = this.getAttribute('compilerOptions');
+                  const compilerOptions = JSON.parse(compilerOptionsStr);
+                } catch(err) {
+                  console.group(`Failed to parse compilerOptions`);
+                  console.groupCollapsed(`compilerOptions`);
+                  console.log(
+                  console.groupEnd();
+                  console.error(err.message);
+                  console.groupEnd();
+                }
+
+                const transpiledCode = ts.transpile(code, {
+                    module: ts.ModuleKind.ESNext,
+                    target: ts.ScriptTarget.ES6,
+                    jsx: ts.JsxEmit.React,
+                    ...compilerOptions
+                });
+
+                const script = document.createElement('script');
+                script.type = 'module';
+                script.textContent = transpiledCode;
+                this.shadow.innerHTML = '';
+                this.shadow.appendChild(script);
+            }
+        }
+
+        customElements.define('typescript', TypeScriptElement);
